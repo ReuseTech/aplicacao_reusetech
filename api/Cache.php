998f82dab902eff4be508d83d7b8e5f0fa84957e
Anthony Muller <anthonyvmuller@outlook.com>
2021-05-25 11:28:08 -0300

    Corrigindo os links da classe Cache

--- api/Cache.php
+++ api/Cache.php
@@ -7,21 +7,21 @@ class Cache{
         $this->selector = new Selector();
     }
     function delete_old_cache(){
-        $file_path = "api/cache/tables_list.json";
+        $file_path = "cache/tables_list.json";
         unlink($file_path) or die("falha ao excluir cache");
-        foreach(glob("api/cache/tabelas/*.json") as $file_path) {
+        foreach(glob("cache/tabelas/*.json") as $file_path) {
             unlink($file_path) or die("falha ao excluir cache");
         }
     }
     function create_tables_list_cache(){
         $tables_json = json_encode($this->selector->get_tables_names());
-        $file_path = "api/cache/tables_list.json";
+        $file_path = "cache/tables_list.json";
         file_put_contents($file_path, $tables_json) or die("Falha ao criar lista de tabelas em arquivo json");
     }
     function create_tables_rows_cache(){
         $tables = $this->selector->get_tables_names();
         foreach($tables as $table){
-            $file_path = "api/cache/tabelas/$table.json";
+            $file_path = "cache/tabelas/$table.json";
             $table_information_json = json_encode($this->selector->get_table_information($this->selector->get_table($table)));
             file_put_contents($file_path, $table_information_json) or die("Falha ao criar informações da tabela $table em arquivo json");
         }
@@ -29,7 +29,7 @@ class Cache{
 }
 
 session_start();
-isset($_SESSION["user"]) or header("location:login.php");
+isset($_SESSION["user"]) or header("location:../php/login.php");
 
 $cache = new Cache();
 
@@ -37,5 +37,5 @@ $cache->delete_old_cache();
 $cache->create_tables_list_cache();
 $cache->create_tables_rows_cache();
 
-header("Location: pages/adicionar.html");
+header("Location:../pages/adicionar.html");
 ?> 
