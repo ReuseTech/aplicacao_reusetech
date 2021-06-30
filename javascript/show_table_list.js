580b7f3489464fb76a703deaa7df2041ca4239d9
Anthony Muller <anthonyvmuller@outlook.com>
2021-06-30 20:03:25 -0300

    Atualizando a página de escolher qual peça será adicionada

--- javascript/show_table_list.js
+++ javascript/show_table_list.js
@@ -1,7 +1,4 @@
-loadTablesList('POST', '../api/cache/tables_list.json')
-    .then((tables_list) => show_tables(tables_list)
-)
-function loadTablesList(method, url) {
+let loadTablesList = (method, url) => {
     return new Promise((resolve, reject) =>{
         const xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
@@ -12,37 +9,55 @@ function loadTablesList(method, url) {
     })
 }
 
-function show_tables(tables_list){
+let showTables = (tables_list) => {
     for (i = 0; i < tables_list.length; i++){
-        let coluna = document.querySelector("div.coluna");
-        coluna.appendChild(createDiv()).
-        appendChild(createA(tables_list[i])).
-        appendChild(createH1(tables_list[i]));            
+        if(tables_list[i] !== 'pc') {
+            let coluna = document.querySelector("div.coluna");
+            coluna.appendChild(createDiv()).
+            appendChild(createA(tables_list[i])).
+            appendChild(createH1(tables_list[i]));  
+        }          
     }
 }
+let showPc = () => {
+    let coluna = document.querySelector("div.coluna");
+    coluna.appendChild(createDiv()).
+    appendChild(createAPc('pc')).
+    appendChild(createH1('pc')); 
+}
 
-function createDiv(){
+let createDiv = () => {
     let div = document.createElement("div");
     div.setAttribute("class", "peca");
+
     return div;
 }
-function createA(table_name){
+let createA = (table_name) => {
     let a = document.createElement("a");
     a.setAttribute("href", "insert_piece.php?table=" + table_name);
     a.setAttribute("class", table_name);
+
     return a;
 }
-function createH1(table_name){
+let createH1 = (table_name) => {
     let h1 = document.createElement("h1"); 
     h1.innerText = table_name.replace("_", " ").replace("_", " ");
+
     return h1;
 }
-function createAPc(){
+let createAPc = () => {
     let a = document.createElement("a");
-    a.setAttribute("href", "adicionar_pc.html");
+    a.setAttribute("href", "adicionarPc.php");
     a.setAttribute("class", 'pc');
+
     return a;
 }
+
+loadTablesList('POST', '../api/cache/tables_list.json')
+    .then((tables_list) => {
+        showPc();
+        showTables(tables_list)
+    })
 
 
 
